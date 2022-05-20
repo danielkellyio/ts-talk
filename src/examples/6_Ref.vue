91f556ecb92549d60b471b036a72e4da996bf3b7
Daniel Kelly <me@danielkelly.io>
2022-05-20 16:11:31 -0500

    more

--- src/examples/6_Ref.vue
+++ src/examples/6_Ref.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-const name = ref(null);
+const name = ref("Daniel Kelly");
 </script>
 
 <template>
--- src/examples/8_Computed.vue
+++ src/examples/8_Computed.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 
-const genrePreference = ref("comedy");
-const movieSuggestion = computed((): string => {
-  const pref = genrePreference.value;
-  const movieMap = {
-    comedy: "Bill and Ted's Excellend Adventure",
-    drama: "Jurassic Park",
-    romance: "Elizabeth Town",
-  };
-  return Object.keys(movieMap).includes(pref) ? movieMap[pref] : "Marvel Movie";
-});
+const num = ref(2);
+const doubled = computed(() => num.value * 2);
 </script>
 
 <template>
-  {{ movieSuggestion }}
+  {{ doubled }}
 </template>
